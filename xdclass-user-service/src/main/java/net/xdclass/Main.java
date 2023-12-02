1552e0e01bef9462ae7136fc9c76cf1621f7533f
烤羊腿 <13011330+roasted-lamb-leg@user.noreply.gitee.com>
2023-12-02 13:38:45 +0800

    项目添加公共包

--- xdclass-user-service/src/main/java/net/xdclass/Main.java
+++ xdclass-user-service/src/main/java/net/xdclass/Main.java
@@ -1,5 +1,11 @@
 package net.xdclass;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
