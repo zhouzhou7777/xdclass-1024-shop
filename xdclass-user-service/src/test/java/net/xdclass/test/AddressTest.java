c860df3437d78ba0d8b8cb159ada9a51ea7e5e04
烤羊腿 <13011330+roasted-lamb-leg@user.noreply.gitee.com>
2023-12-02 15:45:35 +0800

    单元测试增加

---
+++ xdclass-user-service/src/test/java/net/xdclass/test/AddressTest.java
@@ -0,0 +1,31 @@
+package net.xdclass.test;
+
+import lombok.extern.slf4j.Slf4j;
+import net.xdclass.UserApplication;
+import net.xdclass.model.AddressDO;
+import net.xdclass.service.AddressService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserApplication.class)
+@Slf4j
+public class AddressTest {
+
+    @Autowired
+    private AddressService addressService;
+
+    @Test
+    public void testAddressDetail() {
+
+        AddressDO detail = addressService.detail(1L);
+        log.info(detail.toString());
+        Assert.assertNotNull(detail);
+
+    }
+
+}
